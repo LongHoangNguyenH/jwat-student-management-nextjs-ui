e7ac22929e13ca2eae402b0f021c73985ae705f7
longnh <longnh.uit@gmail.com>
2024-12-20 08:36:30 +0700

    feat: create and apply classes form

--- src/components/form/ClassForm.tsx
+++ src/components/form/ClassForm.tsx
@@ -11,10 +11,7 @@ const ClassForm = () => {
           className="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none focus:ring"
           placeholder="Class name"
         />
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded mt-4"
-        >
+        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded mt-4">
           Submit
         </button>
       </div>
